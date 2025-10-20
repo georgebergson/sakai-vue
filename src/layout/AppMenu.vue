710e8f6e0cab394861e207f43c7dda5606460394
George Bergson Castilho <george.castilho@prodam.am.gov.br>
2025-10-20 15:57:15 -0400

    Configuracao do Menu Lateral

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -6,136 +6,302 @@ import AppMenuItem from './AppMenuItem.vue';
 const model = ref([
     {
         label: 'Home',
-        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        items: [{ label: 'PAINEL PRINCIPAL', icon: 'pi pi-fw pi-home', to: '/' }]
     },
     {
-        label: 'UI Components',
-        items: [
-            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-            { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-        ]
+        label: 'ATALHOS',
+        items: [{ label: 'PDV', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' }]
     },
     {
-        label: 'Pages',
+        label: 'MENU',
         icon: 'pi pi-fw pi-briefcase',
-        to: '/pages',
         items: [
             {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
-            },
-            {
-                label: 'Auth',
-                icon: 'pi pi-fw pi-user',
+                label: 'VENDAS',
+                icon: 'pi pi-fw pi-shopping-bag',
                 items: [
                     {
-                        label: 'Login',
+                        label: 'PDV',
+                        icon: 'pi pi-fw pi-users',
+                        to: '/vendas/pdv'
+                    },
+                    {
+                        label: 'Vendas Balcão',
+                        icon: 'pi pi-fw pi-user-plus',
+                        to: '/vendas/vendasbalcao'
+                    },
+                    {
+                        label: 'Ordem de Serviço',
+                        icon: 'pi pi-fw pi-barcode',
+                        to: '/vendas/ordemservico'
+                    },
+                    {
+                        label: 'Comandas',
+                        icon: 'pi pi-fw pi-wallet',
+                        to: '/vendas/comandas'
+                    },
+                    {
+                        label: 'Delivery',
+                        icon: 'pi pi-fw pi-credit-card',
+                        to: '/vendas/delivery'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: '',
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+            {
+                label: 'CADASTROS',
+                icon: 'pi pi-pen-to-square',
+                items: [
+                    {
+                        label: 'Pessoas',
+                        icon: 'pi pi-fw pi-users',
+                        to: '/cadastro/pessoas'
+                    },
+                    {
+                        label: 'Usuarios',
+                        icon: 'pi pi-fw pi-user-plus',
+                        to: '/cadastro/usuarios'
+                    },
+                    {
+                        label: 'Produtos',
+                        icon: 'pi pi-fw pi-barcode',
+                        to: '/cadastro/produtos'
+                    },
+                    {
+                        label: 'Forma de Pagamento',
+                        icon: 'pi pi-fw pi-wallet',
+                        to: '/cadastro/formapagamento'
+                    },
+                    {
+                        label: 'Tipo de Recebimento',
+                        icon: 'pi pi-fw pi-credit-card',
+                        to: '/cadastro/tiporecebimento'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+            {
+                label: 'FINANCEIRO',
+                icon: 'pi pi-building-columns',
+                items: [
+                    {
+                        label: 'Caixa',
+                        icon: 'pi pi-fw pi-dollar',
+                        to: '/cadastro/pessoas'
+                    },
+                    {
+                        label: 'Recebimentos',
+                        icon: 'pi pi-fw pi-receipt',
+                        to: '/cadastro/produtos'
+                    },
+                    {
+                        label: 'Contas a Pagar',
+                        icon: 'pi pi-fw pi-arrow-circle-down',
+                        to: '/cadastro/formapagamento'
+                    },
+                    {
+                        label: 'Contas a Receber',
+                        icon: 'pi pi-fw pi-arrow-circle-up',
+                        to: '/cadastro/tiporecebimento'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+            {
+                label: 'ESTOQUE',
+                icon: 'pi pi-box',
+                items: [
+                    {
+                        label: 'Entrada de Produtos',
                         icon: 'pi pi-fw pi-sign-in',
-                        to: '/auth/login'
+                        to: '/cadastro/pessoas'
                     },
                     {
-                        label: 'Error',
-                        icon: 'pi pi-fw pi-times-circle',
-                        to: '/auth/error'
+                        label: 'Saida de Produtos',
+                        icon: 'pi pi-fw pi-sign-out',
+                        to: '/cadastro/produtos'
                     },
                     {
-                        label: 'Access Denied',
-                        icon: 'pi pi-fw pi-lock',
-                        to: '/auth/access'
-                    }
-                ]
-            },
-            {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/pages/crud'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/pages/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/pages/empty'
-            }
-        ]
-    },
-    {
-        label: 'Hierarchy',
-        items: [
-            {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
-                ]
-            },
-            {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                        label: 'Balanco de Estoque',
+                        icon: 'pi pi-fw pi-sort-amount-up-alt',
+                        to: '/cadastro/formapagamento'
                     }
                 ]
             }
         ]
     },
     {
-        label: 'Get Started',
+        icon: 'pi pi-fw pi-briefcase',
         items: [
             {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-book',
-                to: '/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-github',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
+                label: 'FISCAL',
+                icon: 'pi pi-file-check',
+                items: [
+                    {
+                        label: 'Gerenciamento de Notas',
+                        icon: 'pi pi-fw pi-qrcode',
+                        to: '/cadastro/pessoas'
+                    },
+                    {
+                        label: 'Grupo Tributario',
+                        icon: 'pi pi-fw pi-list-check',
+                        to: '/cadastro/produtos'
+                    },
+                    {
+                        label: 'Configuração de Notas',
+                        icon: 'pi pi-fw pi-wrench',
+                        to: '/cadastro/formapagamento'
+                    },
+                    {
+                        label: 'Enviar para o Contador',
+                        icon: 'pi pi-fw pi-send',
+                        to: '/cadastro/tiporecebimento'
+                    }
+                ]
             }
         ]
     }
+    // {
+    //     label: 'UI Components',
+    //     items: [
+    //         { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+    //         { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+    //         { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+    //         { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+    //         { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+    //         { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+    //         { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+    //         { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+    //         { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+    //         { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
+    //         { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
+    //         { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+    //         { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+    //         { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
+    //         { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
+    //     ]
+    // },
+    // {
+    //     label: 'Pages',
+    //     icon: 'pi pi-fw pi-briefcase',
+    //     to: '/pages',
+    //     items: [
+    //         {
+    //             label: 'Landing',
+    //             icon: 'pi pi-fw pi-globe',
+    //             to: '/landing'
+    //         },
+    //         {
+    //             label: 'Auth',
+    //             icon: 'pi pi-fw pi-user',
+    //             items: [
+    //                 {
+    //                     label: 'Login',
+    //                     icon: 'pi pi-fw pi-sign-in',
+    //                     to: '/auth/login'
+    //                 },
+    //                 {
+    //                     label: 'Error',
+    //                     icon: 'pi pi-fw pi-times-circle',
+    //                     to: '/auth/error'
+    //                 },
+    //                 {
+    //                     label: 'Access Denied',
+    //                     icon: 'pi pi-fw pi-lock',
+    //                     to: '/auth/access'
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             label: 'Crud',
+    //             icon: 'pi pi-fw pi-pencil',
+    //             to: '/pages/crud'
+    //         },
+    //         {
+    //             label: 'Not Found',
+    //             icon: 'pi pi-fw pi-exclamation-circle',
+    //             to: '/pages/notfound'
+    //         },
+    //         {
+    //             label: 'Empty',
+    //             icon: 'pi pi-fw pi-circle-off',
+    //             to: '/pages/empty'
+    //         }
+    //     ]
+    // },
+    // {
+    //     label: 'Hierarchy',
+    //     items: [
+    //         {
+    //             label: 'Submenu 1',
+    //             icon: 'pi pi-fw pi-bookmark',
+    //             items: [
+    //                 {
+    //                     label: 'Submenu 1.1',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [
+    //                         { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+    //                     ]
+    //                 },
+    //                 {
+    //                     label: 'Submenu 1.2',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             label: 'Submenu 2',
+    //             icon: 'pi pi-fw pi-bookmark',
+    //             items: [
+    //                 {
+    //                     label: 'Submenu 2.1',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [
+    //                         { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+    //                     ]
+    //                 },
+    //                 {
+    //                     label: 'Submenu 2.2',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
+    // {
+    //     label: 'Get Started',
+    //     items: [
+    //         {
+    //             label: 'Documentation',
+    //             icon: 'pi pi-fw pi-book',
+    //             to: '/documentation'
+    //         },
+    //         {
+    //             label: 'View Source',
+    //             icon: 'pi pi-fw pi-github',
+    //             url: 'https://github.com/primefaces/sakai-vue',
+    //             target: '_blank'
+    //         }
+    //     ]
+    // }
 ]);
 </script>
 
