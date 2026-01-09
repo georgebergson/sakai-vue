3a3f93e5919839c6d27714d4ce3fffe6986c334a
George Bergson Castilho <george.castilho@prodam.am.gov.br>
2026-01-09 10:21:03 -0400

    Alteracao do painel principal detalhes

--- src/components/dashboard/StatsWidget.vue
+++ src/components/dashboard/StatsWidget.vue
@@ -5,7 +5,7 @@ const dropdownItems = ref([
     { name: 'Semana', code: 2 },
     { name: 'Mês', code: 3 }
 ]);
-const dropdownItem = ref(null);
+const dropdownItem = ref({ name: 'Hoje', code: 1 });
 </script>
 
 <template>
@@ -29,48 +29,60 @@ const dropdownItem = ref(null);
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
-            <div class="flex justify-between mb-4">
+        <div class="card">
+            <div class="flex justify-between">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Notas</span>
-                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">input</div>
+                    <span class="block text-muted-color font-medium mb-8">Notas</span>
                 </div>
                 <div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-dollar text-orange-500 !text-xl"></i>
                 </div>
             </div>
-            <div class="text-primary font-medium text-right">R$ 150.349,32</div>
-            <!-- <span class="text-muted-color"> emitidas</span> -->
+            <div class="text-orange-500 dark:text-surface-0 font-medium text-sm">
+                <div>
+                    <Select id="state" v-model="dropdownItem" :options="dropdownItems" optionLabel="name" placeholder="Selecione" class="w-full bg-red-100"></Select>
+                </div>
+            </div>
+            <div class="text-orange-500 font-medium text-right mt-5 text-xl">R$ 150.349,32</div>
+            <!-- <span class="text-muted-color"> recebidas</span> -->
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
-            <div class="flex justify-between mb-4">
+        <div class="card">
+            <div class="flex justify-between">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Contas a pagar</span>
-                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">input</div>
+                    <span class="block text-muted-color font-medium mb-8">Contas a pagar</span>
                 </div>
                 <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-users text-cyan-500 !text-xl"></i>
                 </div>
             </div>
-            <div class="text-primary font-medium text-right">R$ 150.349,32</div>
-            <!-- <span class="text-muted-color">newly registered</span> -->
+            <div class="text-cyan-500 dark:text-surface-0 font-medium text-sm">
+                <div>
+                    <Select id="state" v-model="dropdownItem" :options="dropdownItems" optionLabel="name" placeholder="Selecione" class="w-full bg-red-100"></Select>
+                </div>
+            </div>
+            <div class="text-cyan-500 font-medium text-right mt-5 text-xl">R$ 150.349,32</div>
+            <!-- <span class="text-muted-color"> recebidas</span> -->
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
-            <div class="flex justify-between mb-4">
+        <div class="card">
+            <div class="flex justify-between">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Contas a receber</span>
-                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">input</div>
+                    <span class="block text-muted-color font-medium mb-8">Contas a receber</span>
                 </div>
                 <div class="flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-comment text-purple-500 !text-xl"></i>
                 </div>
             </div>
-            <div class="text-primary font-medium text-right">R$ 150.349,32</div>
-            <!-- <span class="text-muted-color">responded</span> -->
+            <div class="text-purple-500 dark:text-surface-0 font-medium text-sm">
+                <div>
+                    <Select id="state" v-model="dropdownItem" :options="dropdownItems" optionLabel="name" placeholder="Selecione" class="w-full bg-red-100"></Select>
+                </div>
+            </div>
+            <div class="text-purple-500 font-medium text-right mt-5 text-xl">R$ 150.349,32</div>
+            <!-- <span class="text-muted-color"> recebidas</span> -->
         </div>
     </div>
 </template>
